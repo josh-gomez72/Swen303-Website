794d1e9986988a86a4ef9f5c7b33fefa79172a47
Josh Gomez <josh.gomez72@gmail.com>
2016-03-24 13:15:15 +1300

    Links between pages are working properly, browse exectues some xquery, home page has a description on it, search page has search form

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,34 @@
 var express = require('express');
 var router = express.Router();
+
 var basex = require('basex');
 var client = new basex.Session("127.0.0.1", 1984, "admin", "admin");
+
+client.execute("OPEN Colenso");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Colenso Project' });
 });
 
+router.get('/search', function(req, res, next) {
+  res.render('search', { title: 'Search Database' });
+});
 
-client.execute("OPEN Colenso");
+router.get('/browse', function(req, res, next) {
+	client.execute("XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0';" + "(//name[@type='place'])[1] ",
+	function (error, result) {
+		if (error){ console.error(error);}
+		else {
+		  res.render('browse', { title: 'Browse Database', place: result.result});
+		}
+	}		
+	)
+});
+
+router.get('/upload', function(req, res, next) {
+  res.render('upload', { title: 'Upload/Edit Database' });
+});
+
 
 module.exports = router;
