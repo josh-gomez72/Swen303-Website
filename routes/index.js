5fa8c3b303f882e4659eab23374cbb6971e9da1e
Josh Gomez <josh-gomez72@gmail.com>
2016-03-29 11:58:15 +1300

    Made changes to the style of the navbar and started to work on search

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ var router = express.Router();
 var basex = require('basex');
 var client = new basex.Session("127.0.0.1", 1984, "admin", "admin");
 
+var tei = "XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0';"
+
 client.execute("OPEN Colenso");
 
 /* GET home page. */
@@ -16,7 +18,9 @@ router.get('/search', function(req, res, next) {
 });
 
 router.get('/documentView', function(req, res, next) {
-	client.execute("XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0';" + "(doc('Colenso/"+req.query.document+"'))",
+  console.log(req.query);
+  console.log(req.query.document)
+	client.execute(tei + "(doc('Colenso/"+req.query.document+"'))",
 	function (error, result) {
 		if (error){ console.error(error);}
 		else {
@@ -27,7 +31,7 @@ router.get('/documentView', function(req, res, next) {
 });
 
 router.get('/browse', function (req, res, next){
-	client.execute("XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0';" +"db:list('Colenso')",
+	client.execute(tei +"db:list('Colenso')",
 	function (error, result) {
 		if (error){ console.error(error);}
 		else {
@@ -37,6 +41,14 @@ router.get('/browse', function (req, res, next){
 	}
 	)
 });
+
+router.get('/searchResult', function(req, res, next){
+    var query = req.query.query;
+    console.log(req.req);
+    console.log(query);
+});
+
+
 
 router.get('/upload', function(req, res, next) {
   res.render('upload', { title: 'Upload/Edit Database' });
