c91978e54194113d97a0e49e8e87679b70274222
Josh Gomez <josh-gomez72@gmail.com>
2016-03-29 13:46:09 +1300

    String search now working

--- routes/index.js
+++ routes/index.js
@@ -39,13 +39,25 @@ router.get('/browse', function (req, res, next){
 		  res.render('browse', { title: 'List of Database', place: documentList});
 		}
 	}
-	)
+)
 });
 
 router.get('/searchResult', function(req, res, next){
-    var query = req.query.query;
-    console.log(req.req);
-    console.log(query);
+    console.log(req.query);
+    var stringValue = "'"+ (req.query.stringValue)+ "'";
+    console.log(stringValue);
+    client.execute(tei + "for $v in .//TEI[. contains text "+stringValue+"] return db:path($v)",
+    function (error, result){
+      if (error){
+        console.log("ERRROR");
+        console.error(error);}
+      else {
+       var searchResult = result.result.split('\n');
+       console.log(searchResult);
+       res.render('searchResult', {title: 'Search Results', place: searchResult})
+      }
+    }
+  )
 });
 
 
